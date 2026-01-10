14414b047d0a050348ffb65bc722773f299b90d4
Andy Maskiell <maskiella@gmail.com>
2026-01-10 14:08:49 -0500

    chore: configure react-hooks eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -34,6 +34,8 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
