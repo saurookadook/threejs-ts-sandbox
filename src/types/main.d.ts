cfdc374958d4ab9ff5e1bb456414c8ff78d17a22
Andy Maskiell <maskiella@gmail.com>
2026-05-03 11:58:59 -0400

    feat: add generic types

--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,5 +1,14 @@
 export type Nullable<T> = T | null;
 
+export type KeyedObject<V = unknown, K = string> = {
+  [key in K]: V;
+};
+
+export type ValueOf<T> = T[keyof T];
+
+//-----------------------------------------------------------------------------
+// React
+//-----------------------------------------------------------------------------
 export type ReactCanvasProps = React.CanvasHTMLAttributes<HTMLCanvasElement>;
 export type ReactDivProps = React.PropsWithChildren<
   React.HTMLAttributes<HTMLDivElement>
