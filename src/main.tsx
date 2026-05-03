107f4af5e6c20a920d6037519afc3c6ced462c60
Andy Maskiell <maskiella@gmail.com>
2026-05-03 14:38:42 -0400

    chore: remove StrictMode from main entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const rootEl = document.getElementById('root')!;
+
+createRoot(rootEl).render(<App />);
