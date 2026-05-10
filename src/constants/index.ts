b98382d75eea12bb3cc0523b733e9cb41bd74218
Andy Maskiell <maskiella@gmail.com>
2026-05-10 10:42:20 -0400

    feat: add CameraType constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,10 @@
 export const SPREAD = 15;
 
+export const CameraType = {
+  OrthographicCamera: 'OrthographicCamera',
+  PerspectiveCamera: 'PerspectiveCamera',
+} as const;
+
 export const LightType = {
   AmbientLight: 'AmbientLight',
   DirectionalLight: 'DirectionalLight',
